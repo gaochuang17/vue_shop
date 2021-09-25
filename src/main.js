09a9f20512d09a2f2fb5cac5b8d62bbfd5da8728
gaochuang17 <m17864291492@163.com>
2021-09-25 13:21:23 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -9,9 +9,15 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 
-Vue.prototype.$http=axios
+Vue.prototype.$http = axios
 //配置请求的根路径
-axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  //在最后必须return config
+  return config
+})
 Vue.config.productionTip = axios
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,46 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(Router)
 
 const router = new Router({
-  routes:[
-    {path:'/',redirect:'/login'},
-    {path:'/login',component:Login},
-    {path:'/home',component:Home}
-
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          component: Welcome
+        },
+        {
+          path: '/users',
+          component: Users
+        }
+      ]
+    }
   ]
 })
 
 //挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   //to 将要访问的路径
   //from 代表从哪个路径跳转而来
   //next 是一个函数，表示放行
   //  next（）放行  next('/login') 强制跳转
 
   //如果用户访问的登录页，直接放行
-  if(to.path==='/login') return next();
+  if (to.path === '/login') return next()
   //从sessionStorage中获取到保存的token值
-  const tokenStr=window.sessionStorage.getItem('token')
+  const tokenStr = window.sessionStorage.getItem('token')
   //没有token,强制跳转到登录页
-  if(!tokenStr) return next('/login');
+  if (!tokenStr) return next('/login')
   next()
 })
 
