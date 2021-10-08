9fa1ee94ba54ea05e117cebf17331e6b3cb289d3
gaochuang17 <m17864291492@163.com>
2021-10-08 16:07:45 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 import axios from 'axios'
 
 Vue.prototype.$http = axios
@@ -22,6 +28,23 @@ axios.interceptors.request.use(config => {
 Vue.config.productionTip = axios
 
 Vue.component('tree-table', TreeTable)
+
+//将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import GoodList from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
 
 Vue.use(Router)
 
@@ -43,7 +45,12 @@ const router = new Router({
         {
           path: '/params',
           component: Params
-        }
+        },
+        {
+          path: '/goods',
+          component: GoodList
+        },
+        { path: '/goods/add', component: Add }
       ]
     }
   ]
